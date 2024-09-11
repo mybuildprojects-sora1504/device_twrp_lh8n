3b09095eab9363002496da841c40642221cca6b7
Sora's Playground <kayozzprojects@yandex.com>
2024-09-11 09:15:42 +0800

    Create twrp_LH8n.mk

---
+++ twrp_LH8n.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from LH7n device
+$(call inherit-product, device/tecno/LH7n/device.mk)
+
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Product Specifics
+PRODUCT_NAME := twrp_LH8n
+PRODUCT_DEVICE := LH8n
+PRODUCT_BRAND := Tecno
+PRODUCT_MODEL := Tecno LH8n
+PRODUCT_MANUFACTURER := tecno
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion
