2c924d9ae2351ffb6e7e9ef62e00779671058db0
Sora's Playground <kayozzprojects@yandex.com>
2024-09-11 09:18:54 +0800

    Create AndroidProducts.mk

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_LH8n.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_LH8n-user \
+    twrp_LH8n-userdebug \
+    twrp_LH8n-eng
